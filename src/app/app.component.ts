c60426c37a418cfab33851013733dcbe7ef3eb29
LiborSaja <sajalibor@gmail.com>
2024-09-08 22:21:03 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, Renderer2 } from "@angular/core";
+import { MapComponent } from "./map/map.component";
+import { LogViewerComponent } from "./log-viewer/log-viewer.component";
+import { AboutComponent } from "./about/about.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    standalone: true,
+    imports: [CommonModule, MapComponent, LogViewerComponent, AboutComponent],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'commmotion';
+    currentView: "about" | "map" | "logViewer" = "about"; // přepínání mezi komponentami
+
+    // proměnná pro definování různých pozadí
+    backgrounds = {
+        about: "url(mmm5.jpg)",
+        map: "url(/mmm3.jpg)",
+        logViewer: "url(/mmm2.jpg)",
+    };
+
+    // Metoda pro nastavení aktuálního pozadí
+    getBackground(): string {
+        return this.backgrounds[this.currentView];
+    }
+
+    // přepínání mezi zobrazeními
+    switchComponent(view: "about" | "map" | "logViewer"): void {
+        this.currentView = view;
+
+        console.log("Switching to:", view); // pro debug
+    }
 }
---
+++ src/app/log-viewer/log-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-viewer.component.html',
+  styleUrl: './log-viewer.component.css'
+})
+export class LogViewerComponent {
+
+}
